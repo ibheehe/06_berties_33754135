006301ca4d23c47648ae5ce0b36d105dbddeaea9
ibheehe <ibrahimasif25@gmail.com>
2025-11-26 13:14:39 +0000

    changes

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId ) {
-      res.redirect('./login') // redirect to the login page
+      return res.redirect('/login'); // redirect to the login page
     } else { 
         next (); // move to the next middleware function
     } 
